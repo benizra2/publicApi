6c358481bca6972d8d5785adb8fdc6d049b7e5db
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2018-05-11 16:31:09 -0400

    cleaned up getVisit logic on visitController

--- visitController.js
+++ visitController.js
@@ -12,38 +12,34 @@ module.exports = {
       res.send( { visitId:result._id} );
     })
   }, 
+  
   //expected values either req.visitId or ( req.userId && searchString );
   getVisit(req, res) {
-    console.log("hit getrequest");
-    // console.log("this is request", req);
-    // console.log("this is req.body", req.body);
-    // console.log("this is the url", req.url);
-
     //obtains URL params
     let rawParams = req.url.slice(req.url.indexOf('?') + 1).split('&');
-    console.log("these are rawParams", rawParams);
     //declare obj to store key-val of params
-    let params = {}
+    let params = {};
     //iterate over rawParam array to extract key-val pairs and set it in params obj
     rawParams.map(rawParam => {
         let [key, val] = rawParam.split('=')
         params[key] = decodeURIComponent(val)
-    })
-    console.log("this is params", params);
+    });
+
     //set up findObj. Check if visitId exisits. if it is use that to find in database.    
-    const findObj = {};
+    let findObj = {};
     if (params.visitId) {
       findObj._id = params.visitId;
     //otherwise use userId;
     } else {
       findObj.userId = params.userId;
     }
-    console.log("this is findObj", findObj);
 
-    //query findObj in DB;
-    Visit.find(findObj, (err, result) => {
+    //query findObj in DB; 
+    //sort by _id with -1 means sort => sorts results from newest to oldest based on embedded timestamp in _id
+    Visit.find({findObj}).sort({_id: -1}).exec((err, result) => {
       if (err) res.status(418).send("error");
       console.log("this is the result", result);
+      
       res.send(result);
     });
   }
