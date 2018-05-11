cfd08bf231ce901227557ce1470784cf3088ba5c
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2018-05-11 14:11:13 -0400

    working POST returns expected result

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 const bodyParser = require('body-parser');
+const visitController = require('./visitController');
 
 //declare my local Mongo URI
 const mongoURI = process.env.MONGO_URI || 'mongodb://suarez.ben12:developer12@ds243335.mlab.com:43335/benizra';
@@ -19,5 +20,9 @@ db.once('open', () => {
 //use bodyParser to parse req body 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//define requests with appropriate middleware
+app.post('/visit', visitController.postVisit);
+
 
 app.listen(PORT, () => console.log(`listening to PORT ${PORT}`));
--- visitController.js
+++ visitController.js
@@ -1,12 +1,15 @@
-const Visit = require('./visitModel');
+const Visit = require('./visitModel.js');
 
 module.exports = {
   //post visit using userId and name as key
   postVisit(req, res) {
     Visit.create(req.body, (err, result) => {
       console.log('this is the document to be created', req.body);
-      if (err) return res.status(418).send('error, did not go through');
-      res.send(result);
+      if (err) return res.status(418).send('Error, did not go through');
+      
+      //if successful, returns specified object. Because I'm using mongoDB, 
+      //I'm leveraging mongoDB's self-generated _id assigned to each doc as my visitId
+      res.send( { visitId:result._id} );
     })
   }
 }
