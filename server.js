1e2ee2b88cab4e9968213f1b594e8a466bbd49d3
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2018-05-11 12:59:20 -0400

    set up server and database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const bodyParser = require('body-parser');
+
+//declare my local Mongo URI
+const mongoURI = process.env.MONGO_URI || 'mongodb://suarez.ben12:developer12@ds243335.mlab.com:43335/benizra';
+const PORT = 3000;
+
+//connect to monggoose
+mongoose.connect(mongoURI, { useMongoClient: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to Database');
+});
+
+//use bodyParser to parse req body 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`listening to PORT ${PORT}`));
